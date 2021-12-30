40015731bc9b1bc8b70d4eec7ffe081c93d3a4bd
archernar <michael@fastlight.com>
2021-12-30 07:42:59 -0500

    Update

--- TOY.java
+++ TOY.java
@@ -516,40 +516,6 @@ class Registers {
     }
 }
 
-class Finder {
-    private String  r = "";
-    private Pattern p = null;
-    private Matcher m = null;
-
-    public Finder(String r) {
-        this.r = r;
-        this.p = Pattern.compile(r);
-    }
-    public boolean matches(String sz) {
-        return find(sz);
-    }
-    public boolean isa(String sz) {
-        return find(sz);
-    }
-    public boolean find(String sz) {
-        boolean bRet = false;
-        this.m = this.p.matcher(sz);
-        if (m.find()) {
-            bRet = true;
-        }
-        return bRet;
-    }
-
-    public int getGroupCount() {
-        return this.m.groupCount();
-    }
-    public String get(int n) {
-        String szRet = "";
-        if ( (n>0) && (n <= this.m.groupCount()) ) szRet = new String(this.m.group(n));
-        return szRet;
-
-    }
-}
 
 final class Instruction_Details {
     private int opcode;
